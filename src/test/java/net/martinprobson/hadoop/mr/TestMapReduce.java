6c5d66a59d9ad13491958e3fb7f4ebd72d953340
Martin Robson <martinprobson@gmail.com>
2018-12-11 19:16:14 +0000

    Experiment with cloning a Result/Cells

--- src/test/java/net/martinprobson/hadoop/mr/TestMapReduce.java
+++ src/test/java/net/martinprobson/hadoop/mr/TestMapReduce.java
@@ -1,11 +1,9 @@
 package net.martinprobson.hadoop.mr;
 
-import static junit.framework.TestCase.fail;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.util.ReflectionUtils;
@@ -16,7 +14,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 public class TestMapReduce {
@@ -62,6 +67,29 @@ public class TestMapReduce {
 		Path expectedResultsFile = new Path(p);
 		Map<ImmutableBytesWritable, Result> expectedResults = readSeqFile(conf,expectedResultsFile);
 		Map<ImmutableBytesWritable,Result> actualResults = readSeqFile(conf,new Path(TEST_OUTPUT_DIR + "/part-r-00000"));
+		actualResults.forEach((k,v) -> {
+		    List<Cell> cells = new ArrayList<>();
+			while (v.advance()) {
+				Cell cell = v.current();
+				System.out.println(cell);
+				Cell newCell = CellUtil.createCell(CellUtil.cloneRow(cell),
+						CellUtil.cloneFamily(cell),
+						CellUtil.cloneQualifier(cell),
+						cell.getTimestamp(),
+						cell.getTypeByte(),
+						CellUtil.cloneValue(cell));
+				System.out.println(newCell);
+				assert(cell.equals(newCell));
+				cells.add(newCell);
+			}
+			Result newResult = Result.create(cells);
+			System.out.println(newResult);
+			try {
+				Result.compareResults(newResult, v);
+			} catch (Exception e) {
+				fail("Fail");
+			}
+		});
 		assertEquals(expectedResults.size(), actualResults.size());
 		actualResults.forEach((k,v) -> assertTrue(expectedResults.containsKey(k)));
 		actualResults.forEach((k,v) -> {
